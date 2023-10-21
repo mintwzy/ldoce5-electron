e93d32ed2497cf506e6bf8b602c50808970ecb27
Wang Ziyi <mint.wzy@gmail.com>
2023-10-21 17:18:34 +0800

    we have a basic layout now

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,67 +1,31 @@
 import React from 'react';
-import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import icon from '../../assets/icon.svg';
 import './App.css';
 
-function Hello() {
-  return (
-    <div>
-      <div className="Hello">
-        <img width="200" alt="icon" src={icon} />
-      </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="folded hands">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
-      </div>
-    </div>
-  );
-}
-
 export default function App() {
-  const [content, setContent] = React.useState('');
+  const [content, setContent] = React.useState('hello');
 
   window.electron.ipcRenderer.once('ipc-example', (arg) => {
     // eslint-disable-next-line no-console
-    console.log(arg);
+    console.log(`arg = ${arg}`);
     // @ts-ignore
     setContent(arg);
   });
 
-  const onChange = (event) => {
+  const onChange = (event: any) => {
     event.preventDefault();
 
     window.electron.ipcRenderer.sendMessage('ipc-example', [
       event.target.value,
     ]);
   };
+
   return (
     <div className="main-container">
       <input className="search-bar" onChange={onChange} />
-      <div className="seach-result">{content}</div>
-      <div className="content">main content</div>
+      <div className="content">
+        <div className="word-list">{content}</div>
+        <div className="word-details">main content</div>
+      </div>
     </div>
   );
 }
